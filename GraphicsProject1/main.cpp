eb7c51c2f4dcf6f9fb8c897320a806fb8b955889
daylegacy <bobrovnikovmr@gmail.com>
2019-01-26 18:57:09 +0300

    all is set up

--- GraphicsProject1/main.cpp
+++ GraphicsProject1/main.cpp
@@ -1,8 +1,54 @@
 #include "libs.h"
 #include "libs.h"
 int main() {
+	//INIT GLFW
 	glfwInit();
 
-	system("pause");
+	//CREATE WINDOW
+	const int WINDOW_WIDTH = 640;
+	const int WINDOW_HEIGHT = 480;
+	int framebufferWidth = 0;
+	int framebufferHeight = 0;
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "window", NULL, NULL);
+
+	glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight); 
+	glViewport(0, 0, framebufferWidth, framebufferHeight);
+
+	glfwMakeContextCurrent(window);
+
+
+	//INIT GLEW(NEEDS WINDOW AND OPENGL CONTEXT)
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "ERROR::MAIN.CPP::GLEW_INIT_FAILED" << "\n";
+		glfwTerminate();
+	}
+	//MAIN LOOP
+	while (!glfwWindowShouldClose(window)) {
+		//UPDATE INPUT
+		glfwPollEvents();
+
+		//update
+
+		//Draw
+		//Clear
+		glClearColor(0.f, 0.5f, 0.2f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		//Draw
+
+		//End of draw
+		glfwSwapBuffers(window);
+		glFlush();
+	}
+	//END OF PROGRAM
+	glfwTerminate();
+
 	return 0;
 }

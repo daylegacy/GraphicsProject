ef4e5655bc5f0a5c98638763b6eb4847a1e981fe
daylegacy <bobrovnikovmr@gmail.com>
2019-01-28 19:44:37 +0400

    got texture lgbt-cat

--- GraphicsProject1/main.cpp
+++ GraphicsProject1/main.cpp
@@ -10,7 +10,7 @@ Vertex vertices[] = {
 
 	//glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),	glm::vec2(0.f, 1.f),
 	//glm::vec3(0.5f,-0.5f, 0.f),			glm::vec3(0.f, 0.f, 1.f),	glm::vec2(1.f, 0.f),
-	glm::vec3(0.5f, 0.5f, 0.f),			glm::vec3(1.f, 1.f, 0.f),	glm::vec2(0.f, 0.f)
+	glm::vec3(0.5f, 0.5f, 0.f),			glm::vec3(1.f, 1.f, 0.f),	glm::vec2(1.f, 1.f)
 };
 unsigned nrOfVertices = sizeof(vertices)/sizeof Vertex;
 
@@ -211,6 +211,33 @@ int main() {
 	//bind VAO 0
 	glBindVertexArray(0);
 	
+
+	//texture init
+	int image_width = 0;
+	int image_height = 0;
+	unsigned char* image = SOIL_load_image("Images/pusheen.png", &image_width, &image_height, NULL, SOIL_LOAD_RGBA);
+
+	GLuint texture0;
+	glGenTextures(1, &texture0);
+	glBindTexture(GL_TEXTURE_2D, texture0);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	if (image) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "ERROR::TEXTURE_LOADING_FAILED" << "\n";
+	}
+	glActiveTexture(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image);
+
+
 	//MAIN LOOP
 	while (!glfwWindowShouldClose(window)) {
 		//UPDATE INPUT
@@ -225,6 +252,13 @@ int main() {
 		//Use a program
 		glUseProgram(core_program);
 
+		//update uniforms
+		glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
+
+		//activate texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture0);
+
 		//bind vertex array object
 		glBindVertexArray(VAO);
 
@@ -235,6 +269,11 @@ int main() {
 		//End of draw
 		glfwSwapBuffers(window);
 		glFlush();
+
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glActiveTexture(0);
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 	//END OF PROGRAM
 	glfwDestroyWindow(window);
